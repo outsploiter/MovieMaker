81dfc9183f7ccffcf62e4b42d4e8b27ca2e5653f
outsploiter <suweeharshan@gmail.com>
2023-02-12 20:06:03 +0530

    manual mode added

--- main.py
+++ main.py
@@ -3,7 +3,27 @@ import utils.clip_editor as clipper
 
 
 def manual_mode():
-    pass
+    print('Enter the file location with forward slashes(/) example (file/path/video.mp4)\n\n\tEnter here: ')
+    video_path = input()
+    print('Enter the no of scenes in number: ')
+    num = int(input())
+    time_dict = {}
+    for i in range(num):
+        scene_temp = input('Enter the scene name: ')
+        time_dict[scene_temp] = {}
+        print('Enter the start time(hh:mm:ss)\n\n\tEnter here: ')
+        start_time = input()
+        time_dict[scene_temp]['from'] = start_time
+        print('Enter the start time(hh:mm:ss)\n\n\tEnter here: ')
+        end_time = input()
+        time_dict[scene_temp]['to'] = start_time
+        print('Enter the tag\n\n\tEnter here: ')
+        end_time = input()
+        time_dict[scene_temp]['tag'] = start_time
+
+    clips, tags = clipper.get_short_clips(time_dict, video_path)
+    print('Completed trimming to short video')
+    print('Would you like to upload? : ')
 
 
 def auto_mode():
@@ -13,8 +33,10 @@ def auto_mode():
     video_path = control_dict['file_location'].replace('\\', '/')
     time_dict = control_dict['scenes']
     clips, tags = clipper.get_short_clips(time_dict, video_path)
-    unique_tags = list(set(tags))
-    clipper.merge_clips(clips, tags, unique_tags)
+    print('Completed trimming to short video')
+
+    # unique_tags = list(set(tags))
+    # clipper.merge_clips(clips, tags, unique_tags)
 
 
 def main():
